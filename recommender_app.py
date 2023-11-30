7d7101df1546542f37b0866e56a9192399a1ab0a
tmmarquess <thiago.uchoa18@gmail.com>
2023-11-30 00:03:49 -0400

    adding dropdown instead of user text input

--- recommender_app.py
+++ recommender_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from users import users
+from users import users, subjects
 from recommend import Recommend
 from add_user import add_user
 
@@ -13,12 +13,14 @@ def Recommend_app():
     if username not in users and username != "":
         st.sidebar.title("Inserir novo usuário")
         qnt_materias = st.sidebar.number_input(
-            "quantidade de matérias que deseja avaliar", 1, 5
+            "quantidade de matérias que deseja avaliar", 1, len(subjects)
         )
 
         materias_avaliadas = []
         for i in range(qnt_materias):
-            nome_materia = st.sidebar.text_input("Nome da matéria", key=f"{i}-1")
+            nome_materia = st.sidebar.selectbox(
+                "Nome da matéria", subjects, key=f"{i}-1"
+            )
             avaliacao = st.sidebar.slider(
                 "Nota da matéria", 0.0, 5.0, step=0.5, key=f"{i}-2"
             )
--- users.py
+++ users.py
@@ -5,6 +5,8 @@ file = open("users.csv")
 csv_list = csv.DictReader(file, restkey="nome")
 
 users = dict()
+subjects = list(csv_list.fieldnames)
+subjects.remove("nome")
 
 for user in csv_list:
     users[user["nome"]] = {
